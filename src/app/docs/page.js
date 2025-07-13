ad0c9fe3ec9a95b6f633d961e3509bbb37924cf6
Ir786921 <imran.raza.cs24@iilmcet.ac.in>
2025-07-13 12:55:09 +0530

    done seo of two pages

--- src/app/docs/page.js
+++ src/app/docs/page.js
@@ -1,12 +1,50 @@
 import APIDocumentation from "@/Component/docs";
 
+  export const metadata = {
+  title: "API Documentation | API House",
+  description: "Explore free dummy APIs, endpoints, and usage guides to build and test your applications easily.",
+  keywords: [
+    "api documentation",
+    "dummy api docs",
+    "rest api guide",
+    "api testing tools",
+    "api playground",
+    "postman ready",
+    "mock data"
+  ],
+  openGraph: {
+    title: "API Documentation | API House",
+    description: "Detailed API documentation with request/response examples for developers and testers.",
+    url: "https://api-house-delta.vercel.app/docs",
+    type: "website",
+    images: [
+      {
+        url: "https://api-house-delta.vercel.app/og-docs.jpg",
+        width: 1200,
+        height: 630,
+        alt: "API Docs Cover",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "API Documentation | API House",
+    description: "Quick and easy access to API endpoints with dummy data. Perfect for testing your frontend.",
+    images: ["https://api-house-delta.vercel.app/og-docs.jpg"],
+  },
+  metadataBase: new URL("https://api-house-delta.vercel.app"),
+
+};
+
 
 export default function DocsPage(){
 
     return(
-        <div>
+     <>
+      <div>
       <APIDocumentation/>
         
         </div>
+     </>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,21 @@ import ApiUsageExamples from "@/Component/Home/CodeSnippet";
 import FaqComponent from "@/Component/Home/FaqSection";
 import ApiFeaturesSection from "@/Component/Home/FeatureSection";
 import HeroSection from "@/Component/Home/HeroSection";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <>
+    
+
+      <div>
      <HeroSection/>
      <ApiFeaturesSection/>
      <ApiUsageExamples/>
      <FaqComponent/>
     </div>
+    </>
+   
   );
 }
